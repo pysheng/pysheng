0616dc7da7ce45ee111cb8a0723ea5aceda8e4ed
six <brbsix@gmail.com>
2015-08-19 03:25:10 -0700

    Adjust indentation, quoting, and commas for consistency

--- setup.py
+++ setup.py
@@ -5,30 +5,30 @@ from pysheng import VERSION
 from distutils.core import setup
 import platform
 
-WIN32 = (platform.system() == "Windows")
+WIN32 = (platform.system() == 'Windows')
 
 if WIN32:
     import py2exe
 
 setup_kwargs = dict(
-    name="pysheng",
+    name='pysheng',
     version=VERSION,
-    description="Download books from Google Books",
-    author="Arnau Sanchez",
-    author_email="tokland@gmail.com",
-    url="http://code.google.com/p/Download books from Google Books",
-    packages=[  
-        "pysheng/",
+    description='Download books from Google Books',
+    author='Arnau Sanchez',
+    author_email='tokland@gmail.com',
+    url='http://code.google.com/p/Download books from Google Books',
+    packages=[
+        'pysheng/'
     ],
     scripts=[
-      "bin/pysheng",
-      "bin/pysheng-gui",
-	  ],
-    license="GNU Public License v3.0",
-    long_description=" ".join(__doc__.strip().splitlines()),
+        'bin/pysheng',
+        'bin/pysheng-gui'
+	],
+    license='GNU Public License v3.0',
+    long_description=' '.join(__doc__.strip().splitlines()),
     data_files=[
         ('share/pysheng',
-            ('pysheng/main.glade',)),
+            ('pysheng/main.glade',))
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -38,25 +38,25 @@ setup_kwargs = dict(
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP'
     ],
     options={
         'py2exe' : {
             'packages': 'encodings',
-            'includes': 'cairo, pango, pangocairo, atk, gobject',
+            'includes': 'cairo, pango, pangocairo, atk, gobject'
         },
         'sdist': {
-            'formats': 'zip',
+            'formats': 'zip'
         }
-    }    
+    }
 )
 
 if WIN32:
-    setup_kwargs["windows"] = [
-      {
-        "script": "bin/pysheng-gui",
-        #"icon_resources": [(1, "a.ico")],
-      },
+    setup_kwargs['windows'] = [
+        {
+            'script': 'bin/pysheng-gui',
+            # 'icon_resources': [(1, 'a.ico')]
+        }
     ]
-    
+
 setup(**setup_kwargs)
